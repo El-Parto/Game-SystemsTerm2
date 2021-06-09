512d07a78a8884ec721fffd7388f400253bfc922
El-Parto <luc980@y7mail.com>
2021-06-09 16:28:43 +1000

    Update MainMenuControl.cs

--- Assets/GameSystems  GUI/GUI and GameSystems/Scripts/GUIScripts/MainMenuControl.cs
+++ Assets/GameSystems  GUI/GUI and GameSystems/Scripts/GUIScripts/MainMenuControl.cs
@@ -18,7 +18,7 @@ public class MainMenuControl : MonoBehaviour
     public GameObject optionLoader; //, meant for parenting stuff under
     public GameObject buttLoader; // as generous as it sounds it just loads buttons (the button list outside options
     public Image background;
-    //turns on or off a bunch of butts, ON = play, 
+    //turns on or off a bunch of butt's,
     public void ClickPlay()
     {   
         play.gameObject.SetActive(false);
@@ -73,3 +73,31 @@ public class MainMenuControl : MonoBehaviour
     } 
 
 }
+/*
+ * using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class GameManager : MonoBehaviour
+{
+    public GameObject platformSpawner;
+ 
+    public int platformCount = 500;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 spawnPosition = new Vector3();
+ 
+        for(int i = 0; i < platformCount; i++)
+        {
+            spawnPosition.y += Random.Range(.5f, 2f);
+            spawnPosition.x = Random.Range(-5f, 5f);
+            Instantiate(platformSpawner, spawnPosition, Quaternion.identity);
+        }
+ 
+     //   FindObjectOfType<HighScoreSave>().LoadBinary();
+    }
+ 
+}
+ * 
+*/
